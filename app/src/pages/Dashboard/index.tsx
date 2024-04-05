4fa66ddc377dc0aaa41d9421bbbdc3b67451a956
MarcosLauremiro <lauremiromelo@gmail.com>
2024-04-05 06:56:04 -0300

    atualizando front para re estilizar

---
+++ app/src/pages/Dashboard/index.tsx
@@ -0,0 +1,5 @@
+import { StyleDash } from "./style";
+
+export const Dashboard = () => {
+  return <StyleDash></StyleDash>;
+};
--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,26 +1,9 @@
-import { Events } from "../../components/Events";
-import { Footer } from "../../components/Footer";
-import { HeaderHome } from "../../components/HeaderHome";
-import { History } from "../../components/History";
-import { Line } from "../../components/Line";
-import { Ministery } from "../../components/Ministery";
-import { Pgm } from "../../components/Pgm";
-import { SectionBanner } from "../../components/SectionBanner";
 import { HomeStyle } from "./style";
 
 export const Home = () => {
   return (
     <HomeStyle>
-      <HeaderHome/>
-      <SectionBanner />
-      <History />
-      <Line name="Atividade"/>
-      <Pgm />
-      <Line/>
-      <Ministery />
-      <Line />
-      <Events />
-      <Footer />
+ 
     </HomeStyle>
   );
 };
