60afac3b5fdfd027e9cae295e287d891da73837a
MarcosLauremiro <lauremiromelo@gmail.com>
2024-04-10 20:30:17 -0300

    finalizando controller

--- API/app/src/module/user-detail/user-detail.controller.ts
+++ API/app/src/module/user-detail/user-detail.controller.ts
@@ -1,34 +1,70 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UserDetailService } from './user-detail.service';
 import { CreateUserDetailDto } from './dto/create-user-detail.dto';
 import { UpdateUserDetailDto } from './dto/update-user-detail.dto';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('user-detail')
 export class UserDetailController {
   constructor(private readonly userDetailService: UserDetailService) {}
 
+  @ApiOperation({ summary: 'Criar um novo user detail' })
   @Post()
-  create(@Body() createUserDetailDto: CreateUserDetailDto) {
-    return this.userDetailService.create(createUserDetailDto);
+  create(@Body() createUserDetailDto: CreateUserDetailDto, @Request() req) {
+    const { userId, pgmId, ministryId } = req.body;
+    return this.userDetailService.create(
+      createUserDetailDto,
+      userId,
+      pgmId,
+      ministryId,
+    );
   }
 
+  @ApiOperation({ summary: 'Obter todos os user detail' })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.userDetailService.findAll();
   }
 
+  @ApiOperation({ summary: 'Obter user detail por ID' })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userDetailService.findOne(+id);
+    return this.userDetailService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDetailDto: UpdateUserDetailDto) {
-    return this.userDetailService.update(+id, updateUserDetailDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDetailDto: UpdateUserDetailDto,
+    @Request() req,
+  ) {
+    const { userId, pgmId, ministryId } = req.body;
+    return this.userDetailService.update(
+      id,
+      updateUserDetailDto,
+      userId,
+      pgmId,
+      ministryId,
+    );
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.userDetailService.remove(+id);
+    return this.userDetailService.remove(id);
   }
 }
