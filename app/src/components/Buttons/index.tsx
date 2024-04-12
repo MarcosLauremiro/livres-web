0bf20cb0d814d2ca201d44aa0478ef04d1f714f7
MarcosLauremiro <lauremiromelo@gmail.com>
2024-04-11 22:20:14 -0300

    organizando front end

--- app/src/components/Buttons/index.tsx
+++ app/src/components/Buttons/index.tsx
@@ -6,12 +6,7 @@ export const Buttons = () => {
 
   return (
     <ButtonsStyled>
-      <button className="login" onClick={() => navigate("/login")}>
-        Login
-      </button>
-      <button className="register" onClick={() => navigate("/registro")}>
-        Quero ser familia
-      </button>
+     
     </ButtonsStyled>
   );
 };
